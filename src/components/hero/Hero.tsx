dfb49ac73d1af2c8510650e8449dccc92e61aa7e
Don Alvin <alvindon41@gmail.com>
2023-11-08 11:41:59 +0300

    feat: hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,12 @@
+import { Button } from "../ui/button"
+
 const Hero = () => {
     return (
-        <section className="bg-[url('/images/hero-sm.png')] h-screen bg-no-repeat relative object-cover">
+        <section className="bg-[url('/images/hero-sm.png')] md:bg-[url('/images/hero.png')] h-[600px] bg-no-repeat relative bg-top ">
+            <div className="flex flex-col gap-4 absolute top-[70%] md:top-[75%] left-8">
+                <p className="capitalize italic text-white font-normal bodyXL ">elegance in simplicity, <br /> earth's harmony</p>
+                <Button className="bg-white text-black bodyMD w-fit px-10">New In</Button>
+            </div>
         </section>
     )
 }
